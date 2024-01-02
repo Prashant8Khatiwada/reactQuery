3759d4122101a77ade18e8f26ce1fbe5a4d324b9
Prashant8khatiwada <prashantkhatiwada554@gmail.com>
2024-01-02 22:23:32 +0545

    learnt about caching

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
 import "./App.css";
+import { ReactQueryDevtools } from "react-query/devtools";
 import HomePage from "./components/HomePage";
 import SuperHeroesPage from "./components/SuperHeroesPage";
-import RQSuperHeroesPage from "./components/RQSuperHeroesPage";
+import RQSuperHerosPage from "./components/RQSuperHeroesPage";
 import { QueryClient, QueryClientProvider } from "react-query";
 
 const queryClient = new QueryClient();
@@ -27,11 +28,12 @@ function App() {
         </div>
 
         <Routes>
-          <Route path="/rq-super-heroes" element={<RQSuperHeroesPage />} />
+          <Route path="/rq-super-heroes" element={<RQSuperHerosPage />} />
           <Route path="/super-heroes" element={<SuperHeroesPage />} />
           <Route path="/" element={<HomePage />} />
         </Routes>
       </Router>
+      <ReactQueryDevtools initialIsOpen={false} position="bottom-right" />
     </QueryClientProvider>
   );
 }
